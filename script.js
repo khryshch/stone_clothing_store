8cc6922afbf33b7be30e0ee72d31db6f18a5680c
khryshch <kirillkovalov2005@gmail.com>
2024-12-02 01:39:21 +0200

    Add files via upload
    Загрузка файлов для сайта

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function openForm() {
+    document.getElementById("orderForm").style.display = "flex";
+}
+
+function closeForm() {
+    document.getElementById("orderForm").style.display = "none";
+}
